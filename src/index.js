cd706e540831687573ec0a00971466b7ae29683d
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-02-05 12:31:20 +0200

    complete homewark11

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { getPictures, page, query } from './api';
+import { createMarkup } from './markup';
+import { formRef, galleryRef, loadRef } from './refs';
+
+formRef.addEventListener('submit', onSubmit);
+loadRef.addEventListener('click', onLoadClick);
+
+const lightbox = new SimpleLightbox('.gallery a');
+
+async function onSubmit(event) {
+  event.preventDefault();
+  const searchQuery = event.currentTarget.elements.searchQuery.value
+    .trim()
+    .toLowerCase();
+  if (!searchQuery) {
+    Notify.failure('Enter a search query!');
+    return;
+  }
+  try {
+    const searchData = await getPictures(searchQuery);
+    const { hits, totalHits } = searchData;
+    if (hits.length === 0) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    Notify.success(`Hooray! We found ${totalHits} images!`);
+    const markup = hits.map(item => createMarkup(item)).join('');
+    galleryRef.innerHTML = markup;
+    if (totalHits > 40) {
+      loadRef.classList.remove('js-load-btn');
+      page += 1;
+    }
+    lightbox.refresh();
+  } catch (error) {
+    Notify.failure('Something went wrong! Please retry');
+    console.log(error);
+  }
+}
+
+async function onLoadClick() {
+  const response = await getPictures(query);
+  const { hits, totalHits } = response;
+  const markup = hits.map(item => createMarkup(item)).join('');
+  galleryRef.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+  const amountOfPages = totalHits / 40 - page;
+  if (amountOfPages < 1) {
+    loadRef.classList.add('js-load-btn');
+    Notify.info("We're sorry, but you've reached the end of search results.");
+  }
+}
